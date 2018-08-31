b428faae4fc9e520b12c912b94bcfb39c0dfe7e6
NightCommunity <42852008+NightCommunity@users.noreply.github.com>
2018-08-31 23:22:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,4 +123,52 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('لي�
 
 
 });
+const d = require("discord.js");
+const client = new d.Client();
+var prefix ="*"//:3 غيره بكيفك
+const fs = require("fs");
+var json = JSON.parse(fs.readFileSync("json.json", "utf8"));
+
+client.on("message", (message) => {
+    var command = message.content.split(" ")[0];
+    if (message.content.startsWith(prefix)) return;
+    switch(command) {
+        case "mute" : 
+        if (!message.channel.type =="text") return;
+        if (!message.member.hasPermission("MANAGE_CHANNELS")) return;
+        if (!message.mentions.members.first()) return;
+        message.channel.overwritePermissions(message.mentions.members.first().id,{
+            SEND_MESSAGES : false
+        });
+        json[message.guild.id + message.mentions.members.first()] = {muted : true};
+        fs.writeFile("json.json", JSON.stringify(json), err => {
+            if (err) console.error(err);
+        });
+        message.channel.send("Muted.");
+        break;
+        case "unmute" : 
+        if (!message.channel.type =="text") return;
+        if (!message.member.hasPermission("MANAGE_CHANNELS")) return;
+        if (!message.mentions.members.first()) return;
+        message.channel.overwritePermissions(message.mentions.members.first().id,{
+            SEND_MESSAGES : true
+        });
+        json[message.guild.id + message.mentions.members.first()] = {muted : false};
+        fs.writeFile("json.json", JSON.stringify(json), err => {
+            if (err) console.error(err);
+        });
+        message.channel.send("Unmuted.");
+    }
+});
+client.on("guildMemberAdd", (member) => {
+    if (json[member.guild.id + member.user.id]) {
+        if (json[member.guild.id].muted == true) {
+            member.guild.channels.forEach(c => {
+                c.overwritePermissions(member.id, {
+                    SEND_MESSAGES : false
+                });
+            })
+        };
+    };
+});
 client.login(process.env.BOT_TOKEN);
