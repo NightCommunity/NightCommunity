6b6a32614c164da40cc3188558e0e0080d5b2a42
NightCommunity <42852008+NightCommunity@users.noreply.github.com>
2018-10-06 15:11:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = 'w'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`♛ 3k Soon ♛`,"http://twitch.tv/S-F")
+client.user.setGame(`♛ Road To 3K♛`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
